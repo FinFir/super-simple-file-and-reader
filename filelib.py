f7ef00987c9bbb6603cdf795fe3fcc1786660e4f
FinFir <150492849+FinFir@users.noreply.github.com>
2024-09-02 05:17:39 -0500

    Refine it

--- filelib.py
+++ filelib.py
@@ -3,11 +3,9 @@
 def read_file(filename: str):
   with open(filename, "r") as f:
     content = f.read()
-    f.close()
     return content
 
 def write_file(filename: str, content: str):
   with open(filename, "w") as f:
     f.write(content)
-    f.close()
     return content
